9e65bb4bd048c523b955c19541f8e4c56a0e8e4f
양원석 <wonseokyang@cita.co.kr>
2019-08-05 23:44:14 +0900

    setup

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Content from '../components/Content';
+import Sidebar from '../components/Sidebar';
+
+const Main = () => {
+
+  return (
+    <div id="Container">
+      <Content />
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Main;
